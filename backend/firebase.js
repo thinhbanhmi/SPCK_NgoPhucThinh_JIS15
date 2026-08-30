1a89f94cd75d0e5a6152bb2868dfdab4d8e99eec
Thịnh Bánh Mì <thinhcute210312@gmail.com>
2026-08-30 09:58:37 +0700

    30t8

---
+++ backend/firebase.js
@@ -0,0 +1,20 @@
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+// Cấu hình Firebase lấy từ Console
+const firebaseConfig = {
+  apiKey: "AIzaSyDSf54v-Y8bmCkBBher3FcJuFICt8xH8vw",
+  authDomain: "quan-ao-a181b.firebaseapp.com",
+  projectId: "quan-ao-a181b",
+  storageBucket: "quan-ao-a181b.firebasestorage.app",
+  messagingSenderId: "37816212306",
+  appId: "1:37816212306:web:566265addb39130b05388b",
+  measurementID: "G-E1L8PKM4Z9",
+};
+
+// Khởi tạo ứng dụng
+const app = initializeApp(firebaseConfig);
+
+// Xuất các dịch vụ cần dùng
+export const auth = getAuth(app);
+export const db = getFirestore(app);
